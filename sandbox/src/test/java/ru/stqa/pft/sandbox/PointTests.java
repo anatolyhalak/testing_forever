a11e2c3d282c55446cf8ffafaf2517284532ea82
scsi123 <scsi1234@yandex.ru>
2022-10-06 21:55:10 +0300

    Первые тесты с TestNG на проверку расстояний между точками

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -6,10 +6,24 @@ import static org.testng.Assert.assertEquals;
 public class PointTests {
 
   @Test
-  public void testArea() {
+  public void testDistance1() {
     Point p1 = new Point(5.0, 5.0);
-    Point p2 = new Point(3.0, 6.0);
-    assertEquals(p1.area(5.0, 5.0),25.0);
-    assertEquals(p2.area(3.0, 6.0),18.0);
+    Point p2 = new Point(3.0, 5.0);
+    assertEquals(p2.distance(p1),2.0);
+    System.out.println("Расстояние между точками вычисляется правильно! " + 2 + " = " + 2);
+  }
+  @Test
+  public void testDistance2() {
+    Point p1 = new Point(0.0, 5.0);
+    Point p2 = new Point(-5.0, 5.0);
+    assertEquals(p2.distance(p1),5.0);
+    System.out.println("Расстояние между точками вычисляется правильно! " + 5 + " = " + 5);
+  }
+  @Test
+  public void testDistance3() {
+    Point p1 = new Point(-1.0, 3.0);
+    Point p2 = new Point(-5.0, 7.0);
+    assertEquals(p2.distance(p1),5.656854249492381);
+    System.out.println("Расстояние между точками вычисляется правильно! " + 5.656854249492381 + " = " + 5.656854249492381);
   }
 }
